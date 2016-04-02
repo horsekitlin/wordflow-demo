5fdf4b92cab1eb6eacb0bd1ceb79242254197e72
horsekitlin <horsekit1982@gmail.com>
2016-04-03 00:08:36 +0800

    yield array

--- app1.js
+++ app1.js
@@ -1,12 +1,12 @@
 import co from 'co';
 import * as FBFactory from './fbFactory';
+import { groups } from './config';
 
 co(function* (){
-    const people = yield {
-        tomas : FBFactory.getPerson('468187710027156'),
-        codingbear : FBFactory.getPerson('codingbear')
-    };
-    console.log(people);
+    const pages = yield groups.map((page) => {
+        return FBFactory.getPerson(page.id);
+    });
+    console.log(pages);
 }).catch((err) => {
     console.log(err);
 });
---
+++ config.js
@@ -0,0 +1,22 @@
+export const groups = [
+    {
+      "name": "特工爺爺〈台灣〉",
+      "id": "1130930320256396"
+    },
+    {
+      "name": "林嘉欣 Karena Lam",
+      "id": "117056398392803"
+    },
+    {
+      "name": "Lailai & Chichi",
+      "id": "165964651764"
+    },
+    {
+      "name": "BIO-MAN的NACGM世界",
+      "id": "1552740611609945"
+    },
+    {
+      "name": "NANA MAGIC",
+      "id": "127078804005423"
+    },
+  ]
